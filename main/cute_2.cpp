8e7388aa291bd5a32480414fe395a37ffa2b0c0f
dex <dexterearley@gmail.com>
2026-01-26 03:13:14 -0600

    got the main file ready to be played around with

--- main/cute_2.cpp
+++ main/cute_2.cpp
@@ -1,11 +1,19 @@
+#include "esp32-hal-timer.h"
+#include "esp32-hal.h"
 #include "esp_task_wdt.h"
+#include "esp_timer.h"
+#include "freertos/idf_additions.h"
+#include "soc/gpio_num.h"
+#include "soc/rtc.h"
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <algorithm>
 #include <cstddef>
 #include <cstdio>
 #include <cstring>
+#include <stdlib.h>
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
@@ -35,10 +43,10 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 #include "driver/gpio.h"
 #include "esp_sleep.h"
 
-static const char *TAG = "example";
+static const char *TAG = "cute_2";
 
-#define EXAMPLE_PCNT_HIGH_LIMIT 100
-#define EXAMPLE_PCNT_LOW_LIMIT  -100
+// #define EXAMPLE_PCNT_HIGH_LIMIT 100
+// #define EXAMPLE_PCNT_LOW_LIMIT  -100
 
 #define EXAMPLE_EC11_GPIO_A 18
 #define EXAMPLE_EC11_GPIO_B 19
@@ -46,42 +54,25 @@ static const char *TAG = "example";
 
 pcnt_unit_handle_t pcnt_unit;
 
+// buffer for strings
+char buffer[0xff] = {0};
+char *strings[100];
+
+// len of (strings[])
+int numLines = 0;
+
+// indexes of (strings[])
+int lineSelect = 0;
+int lineSelectScreen = 0;
 
 
-
-// void drawFilenames() {
-  
-//   display.clearDisplay();
-//   display.setTextSize(1);
-//   display.setCursor(0, 0);
-//   display.setTextColor(SSD1306_WHITE);
-
-//   // cursor is upper left corner with adafruit gfx basic font
-//   // ?
-//   int i = fileOffset;
-
-//   while (display.getCursorY() < 64) {
-//     if (i == numFiles) break;
-//     if (i == fileSelect) display.print(">");
-//     display.println(filenames[i]);    
-//     i++;
-//   }
-
-//   if (display.getCursorY() < 64) {
-//     display.println("*");
-//   }
-
-//   display.display();
-// }
-
-
-
+#define LINES_PER_SCREEN 4
 
 
 
 
 void setup() {
-    // ESP_LOGI(TAG, "Initializing SPIFFS");
+    ESP_LOGI(TAG, "Initializing SPIFFS");
 
     esp_vfs_spiffs_conf_t conf = {
       .base_path = "/spiffs",
@@ -96,58 +87,55 @@ void setup() {
 
     if (ret != ESP_OK) {
         if (ret == ESP_FAIL) {
-            // ESP_LOGE(TAG, "Failed to mount or format filesystem");
+            ESP_LOGE(TAG, "Failed to mount or format filesystem");
         } else if (ret == ESP_ERR_NOT_FOUND) {
-            // ESP_LOGE(TAG, "Failed to find SPIFFS partition");
+            ESP_LOGE(TAG, "Failed to find SPIFFS partition");
         } else {
-            // ESP_LOGE(TAG, "Failed to initialize SPIFFS (%s)", esp_err_to_name(ret));
+            ESP_LOGE(TAG, "Failed to initialize SPIFFS (%s)", esp_err_to_name(ret));
         }
         return;
     }
 
-// #ifdef CONFIG_EXAMPLE_SPIFFS_CHECK_ON_START
-    // ESP_LOGI(TAG, "Performing SPIFFS_check().");
+    ESP_LOGI(TAG, "Performing SPIFFS_check().");
     ret = esp_spiffs_check(conf.partition_label);
     if (ret != ESP_OK) {
-        // ESP_LOGE(TAG, "SPIFFS_check() failed (%s)", esp_err_to_name(ret));
+        ESP_LOGE(TAG, "SPIFFS_check() failed (%s)", esp_err_to_name(ret));
         return;
     } else {
-        // ESP_LOGI(TAG, "SPIFFS_check() successful");
+        ESP_LOGI(TAG, "SPIFFS_check() successful");
     }
-// #endif
 
     size_t total = 0, used = 0;
     ret = esp_spiffs_info(conf.partition_label, &total, &used);
     if (ret != ESP_OK) {
-        // ESP_LOGE(TAG, "Failed to get SPIFFS partition information (%s). Formatting...", esp_err_to_name(ret));
+        ESP_LOGE(TAG, "Failed to get SPIFFS partition information (%s). Formatting...", esp_err_to_name(ret));
         esp_spiffs_format(conf.partition_label);
         return;
     } else {
-        // ESP_LOGI(TAG, "Partition size: total: %d, used: %d", total, used);
+        ESP_LOGI(TAG, "Partition size: total: %d, used: %d", total, used);
     }
 
     // Check consistency of reported partition size info.
     if (used > total) {
-        // ESP_LOGW(TAG, "Number of used bytes cannot be larger than total. Performing SPIFFS_check().");
+        ESP_LOGW(TAG, "Number of used bytes cannot be larger than total. Performing SPIFFS_check().");
         ret = esp_spiffs_check(conf.partition_label);
         // Could be also used to mend broken files, to clean unreferenced pages, etc.
         // More info at https://github.com/pellepl/spiffs/wiki/FAQ#powerlosses-contd-when-should-i-run-spiffs_check
         if (ret != ESP_OK) {
-            // ESP_LOGE(TAG, "SPIFFS_check() failed (%s)", esp_err_to_name(ret));
+            ESP_LOGE(TAG, "SPIFFS_check() failed (%s)", esp_err_to_name(ret));
             return;
         } else {
-            // ESP_LOGI(TAG, "SPIFFS_check() successful");
+            ESP_LOGI(TAG, "SPIFFS_check() successful");
         }
     }
 
-    // counter
+
+    // PCNT setup
     ESP_LOGI(TAG, "install pcnt unit");
     pcnt_unit_config_t unit_config = {
-        .low_limit = EXAMPLE_PCNT_LOW_LIMIT,
-        .high_limit = EXAMPLE_PCNT_HIGH_LIMIT,
+        .low_limit = -100,
+        .high_limit = 100,
     };
-    // pcnt_unit_handle_t pcnt_unit = NULL;
-
     ESP_ERROR_CHECK(pcnt_new_unit(&unit_config, &pcnt_unit));
 
     ESP_LOGI(TAG, "set glitch filter");
@@ -176,21 +164,6 @@ void setup() {
     ESP_ERROR_CHECK(pcnt_channel_set_edge_action(pcnt_chan_b, PCNT_CHANNEL_EDGE_ACTION_INCREASE, PCNT_CHANNEL_EDGE_ACTION_DECREASE));
     ESP_ERROR_CHECK(pcnt_channel_set_level_action(pcnt_chan_b, PCNT_CHANNEL_LEVEL_ACTION_KEEP, PCNT_CHANNEL_LEVEL_ACTION_INVERSE));
 
-    // ESP_LOGI(TAG, "add watch points and register callbacks");
-    // int watch_points[] = {EXAMPLE_PCNT_LOW_LIMIT, -50, 0, 50, EXAMPLE_PCNT_HIGH_LIMIT};
-    // for (size_t i = 0; i < sizeof(watch_points) / sizeof(watch_points[0]); i++) {
-    //     ESP_ERROR_CHECK(pcnt_unit_add_watch_point(pcnt_unit, watch_points[i]));
-    // }
-
-
-
-    // pcnt_event_callbacks_t cbs = {
-    //     .on_reach = example_pcnt_on_reach,
-    // };
-    // QueueHandle_t queue = xQueueCreate(10, sizeof(int));
-    // ESP_ERROR_CHECK(pcnt_unit_register_event_callbacks(pcnt_unit, &cbs, queue));
-
-
     ESP_LOGI(TAG, "enable pcnt unit");
     ESP_ERROR_CHECK(pcnt_unit_enable(pcnt_unit));
     ESP_LOGI(TAG, "clear pcnt unit");
@@ -198,57 +171,48 @@ void setup() {
     ESP_LOGI(TAG, "start pcnt unit");
     ESP_ERROR_CHECK(pcnt_unit_start(pcnt_unit));
 
-
-
-
-    // read file
-    // yeah...
-
-    FILE *f = fopen("/spiffs/hello.txt", "r");
+    // write file
+    ESP_LOGI(TAG, "Opening file for write.");
+    FILE *f = fopen("/spiffs/hello.txt", "w");
     if (f == NULL) {
-      return;
+        ESP_LOGE(TAG, "couldn't open /spiffs/hello.txt");
+        return;
     }
+    fprintf(f, "hello\nthis\nis\na\nbunch\nof\nnewlines\nyeah\nsome\nmore\nhere\ntest");
+    fclose(f);
 
 
-    char buffer[0xff] = {0};
-
-    // memset(buffer, int Val, size_t Size)
-
-    if (fread(buffer, 1, 0xff, f) == 0) {
-        // file is empty
+    ESP_LOGI(TAG, "Opening file for read.");
+    f = fopen("/spiffs/hello.txt", "r");
+    if (f == NULL) {
+        ESP_LOGE(TAG, "couldn't open /spiffs/hello.txt");
         return;
     }
 
-
-
-
+    // read file
+    int read;
+    read = fread(buffer, 1, 0xff, f);
+    if (read == 0) {
+        ESP_LOGE(TAG, "no fil?e?");
+        return;
+    }
+    ESP_LOGI(TAG, "read %d bytes from file", read);
     fclose(f);
 
 
 
-
-
-    // Serial.println("hello...");
-
-
-    // Wait for display
-    delay(500);
-
-
     // this should work
-    Wire.setPins(26, 27);
-
     // sda 26
     // scl 27
+    Wire.setPins(26, 27);
 
 
-      // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+    // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
     if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
         // Serial.println(F("SSD1306 allocation failed"));
+        ESP_LOGE(TAG, "screen init failed");
         return;
     }
-
-
 
 
     // Show initial display buffer contents on the screen --
@@ -256,64 +220,120 @@ void setup() {
     display.display();
     delay(2000); // Pause for 2 seconds
 
+
+    // read lines into an array of strings
+    char *line = strtok(buffer, "\n");
+    while (line)
+    {
+        strings[numLines] = line;
+        numLines++;
+        line = strtok(NULL, "\n");
+    }    
+
+
     display.clearDisplay();
     display.setTextSize(1);
     display.setCursor(0, 0);
     display.setTextColor(SSD1306_WHITE);
-    // display.println("hello");
 
-
-
-    // strtok replaces the things
-
-    char *strings[100];
-    int i = 0;
-
-
-    char *line = strtok(buffer, "\n");
-    while (line)
-    {
-        // Serial.println(line);
-        // display.println(line);
-        
-        strings[i] = line;
-        i++;
-
-
-        line = strtok(NULL, "\n");
+    // test....
+    if (numLines) {
+        display.println(strings[0]);
+        display.println(strings[1]);
+        display.println(strings[3]);        
+        display.display();
     }
 
-    display.println(strings[0]);
-    display.println(strings[1]);
-    display.println(strings[3]);
+    // esp_timer_create(const esp_timer_create_args_t *create_args, esp_timer_handle_t *out_handle)
+    
 
 
+    // EC11 channel output high level in normal state, so we set "low level" to wake up the chip
+    // ESP_ERROR_CHECK(gpio_wakeup_enable(EXAMPLE_EC11_GPIO_A, GPIO_INTR_LOW_LEVEL));
+    ESP_ERROR_CHECK(gpio_wakeup_enable(GPIO_NUM_18, GPIO_INTR_LOW_LEVEL));
+    ESP_ERROR_CHECK(esp_sleep_enable_gpio_wakeup());
+    ESP_ERROR_CHECK(esp_light_sleep_start());
+
+    esp_light_sleep_start();
+}
+
+
+
+void drawFilenames() {  
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setCursor(0, 0);
+  display.setTextColor(SSD1306_WHITE);
+
+  // cursor is upper left corner with adafruit gfx basic font
+
+  int i = lineSelectScreen;
+
+  // this will pront off the screen I think
+  while (1) {
+    if (i == numLines) break;
+    if (i == lineSelect) display.print(">");
+    display.println(strings[i]);
+    i++;
+  }
 
     display.display();
-
-
-
-
-
 
 
 }
 
-void loop() {
 
-    // uh
-    esp_task_wdt_reset();
+
+unsigned long sleep_timer = 0.0;
+unsigned long _time = 0;
+
+
+
+void loop() {
+    unsigned long dt = millis() - _time;
+    _time = millis();
+
+    sleep_timer += dt;
+
+    if (sleep_timer >= 10000) {
+        display.dim(true);
+        sleep_timer = 0;
+        esp_light_sleep_start();
+    }
+
+    ESP_LOGI(TAG, "sleep timer: %d; millis: %d", sleep_timer, _time);
+    ESP_LOGI(TAG, "WAKEUP cause: %d", esp_sleep_get_wakeup_cause());
+
+    display.dim(false);
 
     int count;
     pcnt_unit_get_count(pcnt_unit, &count);
 
-    display.clearDisplay();
-    display.setCursor(0, 0);
-    display.println(count);
-    display.display();
+    if (numLines) {
+        if (count < 0) {
+            pcnt_unit_clear_count(pcnt_unit);
+            count = 0;
+        }
 
-    delay(1000);
+        count = count / 4;
 
+        if (count >= numLines) {
+            count = (numLines - 1);
+        }
+
+        lineSelect = count;
+
+        if (lineSelect >= (lineSelectScreen + (LINES_PER_SCREEN - 1))) {
+            lineSelectScreen = lineSelect - (LINES_PER_SCREEN - 1);
+        }
+        else if (lineSelect < lineSelectScreen) {
+            lineSelectScreen = lineSelect;
+        }
+
+        drawFilenames();
+    }
+
+    delay(10);
 }
 
 
